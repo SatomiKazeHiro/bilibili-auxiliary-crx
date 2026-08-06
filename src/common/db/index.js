e720d97453db949550b559fb0db6ea0b978fb5ae
里见风寻 <599894674@qq.com>
2026-08-06 17:57:29 +0800

    🐞 fix: 修复变量导出问题

--- src/common/db/index.js
+++ src/common/db/index.js
@@ -1,4 +1,4 @@
-export { openDB, withStore, DB_NAME, DB_VERSION, VIDEO_STORE, NOTE_STORE } from './core.js';
+export { openDB, withStore, VIDEO_STORE, NOTE_STORE } from './core.js';
 export { getVideo, saveVideo, getAllVideos } from './videos.js';
 export { getNote, saveNote, getAllNotes, clearNotes, batchSaveNotes } from './notes.js';
 export { syncNotesFromSupabase, uploadLocalToSupabase } from './sync.js';
